747b85f67ae04fa2e244628c30b94fcfbe65374d
Niklas Weber <nalderan@gmail.com>
2015-05-29 10:14:13 +0200

    temp

--- aligners.py
+++ aligners.py
@@ -43,21 +43,66 @@ class UniformAligner(AbstractAligner):
     Gives equal time to each word in a text block
     """
 
+    def row2words_dicts(self, row):
+        words_dicts = []
+        t_start = row["t_start"]
+        t_end = row["t_end"]
+        words = sentences2words(row["text"])
+        timediff = t_end - t_start
+        word_time = timediff / len(words)
+        # idea: each word in a sentence uses the same amount of time, approximately
+        for i, word in enumerate(words):
+            words_dicts.append({"t_start": t_start + i * word_time,
+                                "t_end": t_start + (i + 1) * word_time,
+                                "text": word})
+        return words_dicts
+
     def align(self, transcriptions, audio_path=""):
         words_dicts = []
         for i in range(transcriptions.shape[0]):
             row = transcriptions.iloc[i]
-            t_start = row["t_start"]
-            t_end = row["t_end"]
-            words = sentences2words(row["text"])
-            timediff = t_end - t_start
-            word_time = timediff / len(words)
-            # idea: each word in a sentence uses the same amount of time, approximately
-            for i, word in enumerate(words):
-                words_dicts.append({"t_start": t_start + i * word_time,
-                                    "t_end": t_start + (i + 1) * word_time,
-                                    "text": word})
+            words_dicts.extend(self.row2words_dicts(row))
         for word_dict in words_dicts:
             word_dict["text"] = word_dict["text"].lower()
         return words_dicts
 
+
+class WeightedAligner(AbstractAligner):
+    """
+    Time in a block is divided according to word weight
+    """
+
+    def row2words_dicts(self, row):
+
+        weights = [len(word) for word in words]
+        total = sum(weights)
+        weight_fractions = [weight / total for weight in weights]
+        time_fractions = [weight_fraction * timediff for weight_fraction in weight_fractions]
+        offset = t_start
+        for i, word in enumerate(words):
+            words_dicts.append({"t_start": offset,
+                                "t_end": offset + time_fractions[i],
+                                "text": word})
+            offset += time_fractions[i]
+
+        words_dicts = []
+        t_start = row["t_start"]
+        t_end = row["t_end"]
+        words = sentences2words(row["text"])
+        timediff = t_end - t_start
+        word_time = timediff / len(words)
+        # idea: each word in a sentence uses the same amount of time, approximately
+        for i, word in enumerate(words):
+            words_dicts.append({"t_start": t_start + i * word_time,
+                                "t_end": t_start + (i + 1) * word_time,
+                                "text": word})
+        return words_dicts
+
+    def align(self, transcriptions, audio_path=""):
+        words_dicts = []
+        for i in range(transcriptions.shape[0]):
+            row = transcriptions.iloc[i]
+            words_dicts.extend(self.row2words_dicts(row))
+        for word_dict in words_dicts:
+            word_dict["text"] = word_dict["text"].lower()
+        return words_dicts
